f5f9fc0b23e5614d85b0134d529374c2284bf601
agenticaihome <nathanhubert23@gmail.com>
2025-11-25 00:00:02 -0500

    Add Ergo content, Wallet Guide, and deployment config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SalesPage from './SalesPage';
+import HowToBuyErgo from './HowToBuyErgo';
+import WhyErgo from './WhyErgo';
+import WalletGuide from './WalletGuide';
 
 function App() {
-  return <SalesPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SalesPage />} />
+        <Route path="/how-to-buy-ergo" element={<HowToBuyErgo />} />
+        <Route path="/why-ergo" element={<WhyErgo />} />
+        <Route path="/wallet-guide" element={<WalletGuide />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
